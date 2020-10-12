f6a96920098e4c1fb5bad14bd7e063aea61a4a90
tovebr <tove.brandt@student.kyh.se>
2020-10-12 13:27:19 +0200

    desktop-ready

---
+++ script.js
@@ -0,0 +1,56 @@
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
+const submitBtn = document.getElementById('submit');
+
+nameInput.addEventListener('input', evt => textInputValidation(evt.target));
+emailInput.addEventListener('input', evt => emailValidation(evt.target));
+messageInput.addEventListener('input', evt => textInputValidation(evt.target));
+submitBtn.addEventListener('click', evt => validateInput(evt));
+
+function textInputValidation(target) {
+    const formSection = target.parentElement;
+
+    if (target.value === '') {
+        formSection.classList = 'form-section error';
+        showError(formSection, false);
+    } else {
+        formSection.classList = 'form-section success';
+    }
+}
+
+function emailValidation(target) {
+    const formSection = target.parentElement;
+
+    // varför i helvete funkar inte detta!?
+    // const userEmail = target.value;
+    if (emailInput.value === '') {
+        formSection.classList = 'form-section error';
+        showError(formSection, false)
+    } else if (!emailInput.validity.valid) {
+        formSection.classList = 'form-section error';
+        showError(formSection, 'Email must be valid');
+    } else {
+        formSection.classList = 'form-section success';
+    }
+}
+
+function showError(target, message) {
+
+    message === false ? message = 'Must be filled out' : message = message;
+
+    const errorContainer = target.querySelector('.error-message');
+    errorContainer.textContent = message;
+}
+
+function validateInput(evt) {
+    textInputValidation(nameInput);
+    textInputValidation(messageInput);
+    emailValidation(emailInput);
+
+    const errors = document.querySelectorAll('.error');
+    const errorArr = Array.from(errors);
+    if(errorArr.length > 0) {
+        evt.preventDefault();
+    }
+}

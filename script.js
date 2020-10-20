cb3c65e11049ba6369941e768005b6a459f968eb
tovebr <tove.brandt@student.kyh.se>
2020-10-20 09:41:13 +0200

    final touches

--- script.js
+++ script.js
@@ -18,32 +18,32 @@ let formValidation = (function() {
 
         /* Preventing submit-eventListener puting error-class on parent(which is <form>) */
         if(target.type !== 'submit') {
-        const formSection = target.parentElement;
+            const formSection = target.parentElement;
 
-        if (target.value === '') {
-            if(formSection.classList.contains('success')) { 
-                formSection.classList.remove('success');
-            }
-            formSection.classList.add('error');
-            showError(formSection, false);
-        } else if (target.type === 'email') {
-
-            if(!emailInput.validity.valid) {
+            if (target.value === '') {
                 if(formSection.classList.contains('success')) { 
                     formSection.classList.remove('success');
                 }
-                formSection.classList.add('error')
-                showError(formSection, 'Email must be valid');
+                formSection.classList.add('error');
+                showError(formSection, false);
+            } else if (target.type === 'email') {
+
+                if(!emailInput.validity.valid) {
+                    if(formSection.classList.contains('success')) { 
+                        formSection.classList.remove('success');
+                    }
+                    formSection.classList.add('error')
+                    showError(formSection, 'Email must be valid');
+                } else {
+                    formSection.classList.remove('error');
+                    formSection.classList.add('success');
+                }
+
             } else {
                 formSection.classList.remove('error');
                 formSection.classList.add('success');
             }
-
-        } else {
-            formSection.classList.remove('error');
-            formSection.classList.add('success');
         }
-    }
     }
 
     const showError = (target, message) => target.querySelector('.error-message').textContent = !message ? 'Must be filled out' : message;
@@ -70,7 +70,7 @@ let menuCtrl = (function() {
         const menuUL = document.querySelector('ul');
         const menuCheckbox = document.querySelector('input[type="checkbox"]');
 
-        menuUL.addEventListener('click', evt => {
+        menuUL.addEventListener('click', () => {
             menuCheckbox.checked = false;
         });
     }
